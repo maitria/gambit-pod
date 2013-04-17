c798690fd6918f473a234090147b261bba2f70e7
Jason Felice <jason.m.felice@gmail.com>
2013-04-16 20:25:32 -0400

    Update script to build compiler

--- build.sh
+++ build.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
-DIRS='include lib'
-
-../gambit/misc/build-gambit-iOS --no-update || exit $?
+gambit_build_script='../gambit/misc/build-gambit-iOS'
+$gambit_build_script --no-update || exit $?
 
 rm -rf include lib
 mkdir -p include lib
 (cd gambit-iOS/current; find lib -name 'lib*.a' |cpio -p ../..)
 (cd gambit-iOS/current; find include -name '*.h' |cpio -p ../..)
+
+gambit_source_dir=`sed -n 's/^gambit_dist_if_downloaded="\(.*\)"/\1/p' $gambit_build_script`
+compiler_dir="`pwd`/compiler"
+
+rm -rf "$gambit_source_dir"
+tar xzf "$gambit_source_dir.tgz"
+cd "$gambit_source_dir"
+
+./configure --prefix="$compiler_dir"
+make clean
+make -j 2
+make install
+
+cd ..
